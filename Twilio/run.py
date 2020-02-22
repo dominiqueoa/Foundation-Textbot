58e6cbd982aee29887a21a8ac174ece7f040233f
Oblackmon <blackmoniii.odell@gmail.com>
2020-02-22 16:19:05 -0500

    Textbot is capable of outputting terms and definitions based on input

--- Twilio/run.py
+++ Twilio/run.py
@@ -1,18 +1,23 @@
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
-from flask import Flask, request
+from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+flashCards = {"A": ("term1", "definition1"), "B" : ("term2", "definition2"), "C": ("term3", "definition3"), "D": ("term4", "definition4")}
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_ahoy_reply():
+def incoming_SMS():
     """Respond to incoming messages with a friendly SMS."""
     # Start our response
     resp = MessagingResponse()
-
+    body = request.values.get('Body', None)
     # Add a message
-    resp.message("Hello! Welcome to Eko's SMS Dictionary!")
+
+    if body in flashCards:
+    	resp.message(flashCards[body][0] + ": " + flashCards[body][1])
+    else:
+    	resp.message("Hello! Welcome to Eko's SMS Flashcards! Please enter A, B, C, or D to see some of our terms")
 
     return str(resp)
 
