bacff8f1a0d8ae6a4a1e27719c639a1b357da954
Oblackmon <oblackmo@andrew.cmu.edu>
2020-04-18 15:57:14 -0400

    Removed Odell's user information

--- Twilio/send_sms.py
+++ Twilio/send_sms.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC6925242ba72859741e231862093599d0"
+account_sid = "-----------------------------------"
 # Your Auth Token from twilio.com/console
-auth_token  = "402f584311810577d63a1a9216f5c79a"
+auth_token  = "---------------------------------"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+14048607166", 
-    from_="++12054486159",
+    to="---------", 
+    from_="-----------",
     body="Hello from Python!")
 
 print(message.sid)
